9682a8cb33ca872249d69ec1daa308847cfcc2eb
Marcelo R Filho <marcelo.1496@gmail.com>
2023-08-23 16:36:39 -0400

    MenuItems

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,5 +1,46 @@
 package gui;
 
-public class MainViewController {
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable{
+	
+	@FXML
+	private MenuItem miSeller;
+	
+	@FXML
+	private MenuItem miDepartment;
+	
+	@FXML
+	private MenuItem miAbout;
+	
+	@FXML
+	public void onMISellerAction() {
+		System.out.println("MI Seller");
+	}
+	
+	@FXML
+	public void onMIDepartmentAction() {
+		System.out.println("MI Department");
+	}
+	
+	@FXML
+	public void onMIAboutAction() {
+		System.out.println("MI About");
+	}
+	
+	
+	
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
